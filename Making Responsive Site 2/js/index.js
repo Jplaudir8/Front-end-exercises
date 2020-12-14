113f1d601a0d0b3ac7b73fa09d9a0b94c0d6d72c
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-12-14 18:33:55 -0500

    added some comments

--- Making Responsive Site 2/js/index.js
+++ Making Responsive Site 2/js/index.js
@@ -9,16 +9,24 @@ emailCollectorForm.addEventListener("submit", event => {
     let userFirstName = ourFormData.get("firstName");
     let userEmailAddress = ourFormData.get("emailAddress");
 
+    // OR... instead of using the FormData object we could have also used the value property that
+    // these html elements have:
+    // let userFirstName = document.getElementById("firstName").value;
+    // let userEmailAddress = document.getElementById("email").value;
+
     console.log(userFirstName);
     console.log(userEmailAddress);
 
-    let updatedHtmlContent = `
-    
+    let newHtmlContent = `
         <h2>Congratulations, ${userFirstName}!</h2>
         <p>You're on your way to becoming a BBQ Master!</p>
         <p class="fine-print">You will get weekly BBQ tips sent to: ${userEmailAddress}</p>`;
 
     let updatedContentContainer = document.getElementById("Main-Content");
-    updatedContentContainer.innerHTML = updatedHtmlContent;
+    updatedContentContainer.innerHTML = newHtmlContent; 
+    
+    // Remember that when want to insert text content inside an html element, rather than 
+    // using .innerHTML (which can have security risks) or .innerText(expensive to load) we
+    // can use .textContent.
 
 })
