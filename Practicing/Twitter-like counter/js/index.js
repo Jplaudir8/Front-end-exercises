2608b7778cc01d1825a8232c1b858e07e93125d0
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-12-23 12:12:53 -0500

    finished twitter component

--- Practicing/Twitter-like counter/js/index.js
+++ Practicing/Twitter-like counter/js/index.js
@@ -5,15 +5,35 @@ textArea.addEventListener("keyup", function() {
     // var maxLength = parseInt($this.getAttribute('maxlength'));
     var maxLength = 140;
     var currentInputLength = textArea.value.length;
+    var currentInputContent = textArea.value;
     var currentCount = maxLength - currentInputLength;
     
-    
     var counterElement = document.getElementById("counterFooter");
+    let tweetBtn = document.getElementById("btn");
     // Styling up before printing
-    if (maxLength <= currentInputLength) {
+    if (maxLength < currentInputLength) {
+        // Turn red
         counterElement.style.color = "red";
-    } else if (maxLength > currentInputLength){
-        counterElement.style.color = "black";
+
+        // Disable tweet button
+        tweetBtn.setAttribute("disabled", "");
+
+        // add .buttonDisabled class. Remember classList.add() already 
+        // has a built-in check so if the class we intend to add 
+        // is already included, then it ignores it. same with classList.remove()
+        tweetBtn.classList.add("buttonDisabled");
+    } else if (maxLength >= currentInputLength){
+        // Turn/keep white
+        counterElement.style.color = "white";
+        
+        // Re-enable by removing disabled attribute
+        tweetBtn.removeAttribute("disabled");
+
+        // Remove .buttonDisabled class.
+        tweetBtn.classList.remove("buttonDisabled");
+
+        // Update onclick link
+        tweetBtn.setAttribute("onclick", "window.location.href='https://twitter.com/intent/tweet?text="+ currentInputContent +"';");
     }
     
     counterElement.textContent = currentCount + "/" + maxLength;
